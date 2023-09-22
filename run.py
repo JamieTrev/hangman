a4512f89c945008395d6f32c49efc026d067fa6a
JamieTrev <>
2023-09-22 10:30:16 +0000

    Add print hangman function to display the hangman's progress

--- run.py
+++ run.py
@@ -8,3 +8,51 @@ wordDictionary = ["project", "monkey", "book", "sunflower", "coffee", "mouse", "
 
 ### Choose a random word
 randomWord = random.choice(wordDictionary)
+
+for x in randomWord:
+    print("_", end=" ")
+
+def print_hangman(wrong):
+    if(wrong == 0):
+        print("\n+---+")
+        print("    |")
+        print("    |")
+        print("    |")
+        print("   ===")
+    elif(wrong == 1):
+        print("\n+---+")
+        print("O   |")
+        print("    |")
+        print("    |")
+        print("   ===")
+    elif(wrong == 2):
+        print("\n+---+")
+        print("O   |")
+        print("|   |")
+        print("    |")
+        print("   ===")
+    elif(wrong == 3):
+        print("\n+---+")
+        print(" O   |")
+        print("/|   |")
+        print("     |")
+        print("    ===")
+    elif(wrong == 4):
+        print("\n+---+")
+        print(" O   |")
+        print("/|\  |")
+        print("     |")
+        print("    ===")
+    elif(wrong == 5):
+        print("\n+---+")
+        print(" O   |")
+        print("/|\  |")
+        print("/    |")
+        print("    ===")
+    elif(wrong == 6):
+        print("\n+---+")
+        print(" O   |")
+        print("/|\  |")
+        print("/ \  |")
+        print("    ===")
+
