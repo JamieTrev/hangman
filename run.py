9701a0c33eaad2fc2be44c1d67b29c7ddb2d0686
JamieTrev <>
2023-09-22 13:57:48 +0000

    Update the loop to fix bug

--- run.py
+++ run.py
@@ -18,99 +18,101 @@ wordDictionary = [
 
 ### Choose a random word
 randomWord = random.choice(wordDictionary)
+randomWord = randomWord.lower()  # Convert the word to lowercase for case-insensitive matching
 
 for x in randomWord:
     print("_", end=" ")
 
 def print_hangman(wrong):
-    if(wrong == 0):
-        print("\n+---+")
-        print("    |")
-        print("    |")
-        print("    |")
-        print("   ===")
-    elif(wrong == 1):
-        print("\n+---+")
-        print("O   |")
-        print("    |")
-        print("    |")
-        print("   ===")
-    elif(wrong == 2):
-        print("\n+---+")
-        print("O   |")
-        print("|   |")
-        print("    |")
-        print("   ===")
-    elif(wrong == 3):
-        print("\n+---+")
-        print(" O   |")
-        print("/|   |")
-        print("     |")
-        print("    ===")
-    elif(wrong == 4):
-        print("\n+---+")
-        print(" O   |")
-        print("/|\  |")
-        print("     |")
-        print("    ===")
-    elif(wrong == 5):
-        print("\n+---+")
-        print(" O   |")
-        print("/|\  |")
-        print("/    |")
-        print("    ===")
-    elif(wrong == 6):
-        print("\n+---+")
-        print(" O   |")
-        print("/|\  |")
-        print("/ \  |")
-        print("    ===")
+    hangman_images = [
+        """
+        +---+
+            |
+            |
+            |
+           ===
+        """,
+        """
+        +---+
+        O   |
+            |
+            |
+           ===
+        """,
+        """
+        +---+
+        O   |
+        |   |
+            |
+           ===
+        """,
+        """
+        +---+
+        O   |
+       /|   |
+            |
+           ===
+        """,
+        """
+        +---+
+        O   |
+       /|\\  |
+            |
+           ===
+        """,
+        """
+        +---+
+        O   |
+       /|\\  |
+       /    |
+           ===
+        """,
+        """
+        +---+
+        O   |
+       /|\\  |
+       / \\  |
+           ===
+        """
+    ]
+    print(hangman_images[wrong])
 
 def printWord(guessedLetters):
-    counter= 0
-    rightLetters= 0
     for char in randomWord:
-        if(char in guessedLetters):
-            print(randomWord[counter], end=" ")
-            rightLetters+=1
+        if char in guessedLetters:
+            print(char, end=" ")
         else:
-            print(" ", end=" ")
-        counter+=1
-    return rightLetters
+            print("_", end=" ")
 
 def printLines():
-    print("\r")
-    for char in randomWord:
-        print("\u203E", end=" ")
+    print("\n" + "\u203E " * len(randomWord))
 
 length_of_word_to_guess = len(randomWord)
 amount_of_times_wrong = 0
-current_guess_index = 0
 current_letters_guessed = []
 current_letters_right = 0
 
-while(amount_of_times_wrong != 6 and current_letters_right != length_of_word_to_guess):
+while amount_of_times_wrong != 6 and current_letters_right != length_of_word_to_guess:
     print("\nLetters guessed so far: ")
     for letter in current_letters_guessed:
         print(letter, end=" ")
     ### Prompt user for input
-    letterGuessed = input("\nGuess a letter: ")
+    letterGuessed = input("\nGuess a letter: ").lower()  # Convert the guess to lowercase for case-insensitive matching
+    ### Check if the letter has already been guessed
+    if letterGuessed in current_letters_guessed:
+        print("You have already guessed that letter.")
+        continue
+    current_letters_guessed.append(letterGuessed)
     ### User is right
-    if(randomWord[current_guess_index] == letterGuessed):
-        print_hangman(amount_of_times_wrong)
-        ### Print word
-        current_guess_index+=1
-        current_letters_guessed.append(letterGuessed)
+    if letterGuessed in randomWord:
         current_letters_right = printWord(current_letters_guessed)
         printLines()
     ### User is wrong
     else:
-        amount_of_times_wrong+=1
-        current_letters_guessed.append(letterGuessed)
+        amount_of_times_wrong += 1
         ### Update the drawing
         print_hangman(amount_of_times_wrong)
-        ### Print word
-        current_letters_right = printWord(current_letters_guessed)
+        printWord(current_letters_guessed)
         printLines()
 
 print("Game is over! Thank you for playing :)")
