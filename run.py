1a3a4f05e89f7e2fbb43188354b0b1c13310ab68
JamieTrev <>
2023-09-23 12:07:14 +0000

    Fixing the 38 various 'Problems'

--- run.py
+++ run.py
@@ -4,24 +4,27 @@ print("Welcome to Hangman")
 print("----------------------------------------")
 
 wordDictionary = [
-    "project", 
-    "monkey", 
-    "book", 
-    "sunflower", 
+    "project",
+    "monkey",
+    "python",
+    "sunflower",
     "coffee",
-    "mouse", 
-    "picture", 
-    "school", 
-    "sandwich", 
+    "mouse",
+    "picture",
+    "school",
+    "sandwich",
     "definition"
 ]
 
-### Choose a random word
+"""
+Choose a random word
+"""
 randomWord = random.choice(wordDictionary)
-randomWord = randomWord.lower()  # Convert the word to lowercase for case-insensitive matching
+randomWord = randomWord.lower()
 
 for x in randomWord:
     print("_", end=" ")
+
 
 def print_hangman(wrong):
     hangman_images = [
@@ -56,26 +59,27 @@ def print_hangman(wrong):
         """
         +---+
         O   |
-       /|\\  |
+       /|\  |
             |
            ===
         """,
         """
         +---+
         O   |
-       /|\\  |
+       /|\  |
        /    |
            ===
         """,
         """
         +---+
         O   |
-       /|\\  |
-       / \\  |
+       /|\  |
+       / \  |
            ===
         """
     ]
     print(hangman_images[wrong])
+
 
 def printWord(guessedLetters):
     for char in randomWord:
@@ -84,33 +88,41 @@ def printWord(guessedLetters):
         else:
             print("_", end=" ")
 
+
 def printLines():
     print("\n" + "\u203E " * len(randomWord))
+
 
 length_of_word_to_guess = len(randomWord)
 amount_of_times_wrong = 0
 current_letters_guessed = []
 current_letters_right = 0
 
-while amount_of_times_wrong != 6 and current_letters_right != length_of_word_to_guess:
+while amount_of_times_wrong != 6 and current_letters_right != length_of_word_to_guess:  # noqa
     print("\nLetters guessed so far: ")
     for letter in current_letters_guessed:
         print(letter, end=" ")
-    ### Prompt user for input
-    letterGuessed = input("\nGuess a letter: ").lower()  # Convert the guess to lowercase for case-insensitive matching
-    ### Check if the letter has already been guessed
+    """
+    Prompt user for input
+    """
+    letterGuessed = input("\nGuess a letter: ").lower()
+    """
+    Check if the letter has already been guessed
+    """
     if letterGuessed in current_letters_guessed:
         print("You have already guessed that letter.")
         continue
     current_letters_guessed.append(letterGuessed)
-    ### User is right
+
     if letterGuessed in randomWord:
-        current_letters_right = printWord(current_letters_guessed)
+        printWord(current_letters_guessed)
         printLines()
-    ### User is wrong
+
     else:
         amount_of_times_wrong += 1
-        ### Update the drawing
+        """
+        Update the drawing
+        """
         print_hangman(amount_of_times_wrong)
         printWord(current_letters_guessed)
         printLines()
