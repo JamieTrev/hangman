b078d16265615af13e5942bc16e974985470e59a
JamieTrev <>
2023-09-23 13:46:21 +0000

    Add a break to game for correctly guessing the word

--- run.py
+++ run.py
@@ -16,9 +16,6 @@ wordDictionary = [
     "definition"
 ]
 
-"""
-Choose a random word
-"""
 randomWord = random.choice(wordDictionary)
 randomWord = randomWord.lower()
 
@@ -98,33 +95,33 @@ amount_of_times_wrong = 0
 current_letters_guessed = []
 current_letters_right = 0
 
-while amount_of_times_wrong != 6 and current_letters_right != length_of_word_to_guess:  # noqa
+while amount_of_times_wrong != 6:
     print("\nLetters guessed so far: ")
     for letter in current_letters_guessed:
         print(letter, end=" ")
-    """
-    Prompt user for input
-    """
+
     letterGuessed = input("\nGuess a letter: ").lower()
-    """
-    Check if the letter has already been guessed
-    """
+
     if letterGuessed in current_letters_guessed:
         print("You have already guessed that letter.")
         continue
     current_letters_guessed.append(letterGuessed)
 
     if letterGuessed in randomWord:
+        current_letters_right = sum(1 for char in randomWord if char in current_letters_guessed)  # noqa
         printWord(current_letters_guessed)
         printLines()
 
+        if current_letters_right == length_of_word_to_guess:
+            print("\nCongratulations! You've guessed the word: " + randomWord)
+            break  # Exit the loop as the game is won
     else:
         amount_of_times_wrong += 1
-        """
-        Update the drawing
-        """
         print_hangman(amount_of_times_wrong)
         printWord(current_letters_guessed)
         printLines()
+
+if amount_of_times_wrong == 6:
+    print("\nSorry, you've run out of attempts. The word was:", randomWord)
 
 print("Game is over! Thank you for playing :)")
