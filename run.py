90d5582b3210cd09b9939fbc594ef3e4be4227eb
JamieTrev <>
2023-11-08 16:58:43 +0000

    Add a function to allow user to play again

--- run.py
+++ run.py
@@ -35,11 +35,6 @@ WORD_DICTIONARY = [
 
 
 def print_hangman(wrong):
-    """Prints the hangman based on the number of incorrect guesses.
-
-    Args:
-        wrong (int): The number of incorrect guesses made by the player.
-    """
     hangman_images = [
         """
         +---+
@@ -94,54 +89,25 @@ def print_hangman(wrong):
     print(hangman_images[wrong])
 
 
-def print_current_word(current_word, guessedLetters):
-    """Prints the current word with correctly guessed letters
-    and underscores for unguessed letters.
-
-    Args:
-        current_word (str): The word to be guessed.
-        guessedLetters (list[str]): List of letters guessed by the player.
-    """
+def print_current_word(current_word, guessed_letters):
     for char in current_word:
-        if char in guessedLetters:
+        if char in guessed_letters:
             print(char, end=" ")
         else:
             print("_", end=" ")
 
 
 def print_empty_lines(current_word):
-    """Prints empty lines to separate word display.
-
-    Args:
-        current_word (str): The word to be guessed.
-    """
     print("\n" + "\u203E " * len(current_word))
 
 
 def print_current_guessed(current_letters_guessed):
-    """Prints the letters guessed so far by the player.
-
-    Args:
-        current_letters_guessed (list[str]):
-        List of letters guessed by the player.
-    """
     print("\nLetters guessed so far: ")
     for letter in current_letters_guessed:
         print(letter, end=" ")
 
 
 def input_next_guess(current_letters_guessed):
-    """Accepts user input and makes sure it is valid.
-
-    Keeps asking for user input unless user enters a valid input.
-
-    Args:
-        current_letters_guessed (list[string]):
-        List of currently guessed user inputs.
-
-    Returns:
-        string: A valid user input.
-    """
     while True:
         user_input = input("\nGuess a letter: ").lower()
 
@@ -160,71 +126,57 @@ def input_next_guess(current_letters_guessed):
         return user_input
 
 
-def check_guessed_word(
-    current_letters_guessed, current_word, user_input, current_letters_right
-):
-    """Checks if the guessed word is correct and updates the game state.
-
-    Args:
-        current_letters_guessed (list[str]):
-        List of letters guessed by the player.
-        current_word (str): The word to be guessed.
-        user_input (str): The user's input guess.
-        current_letters_right (int):
-        The number of letters guessed correctly so far.
-
-    Returns:
-        True if the guessed word is correct, False otherwise.
-    """
+def check_guessed_word(current_letters_guessed, current_word, user_input):
     current_letters_guessed.append(user_input)
 
-    print_current_word(current_word, current_letters_guessed)
-    print_empty_lines(current_word)
     if user_input in current_word:
-        current_letters_right = sum(
-            1 for char in current_word if char in current_letters_guessed
-        )  # noqa
+        current_letters_right = sum(1 for char in current_word if char in current_letters_guessed)
 
         if current_letters_right == len(current_word):
-            print("\nCongratulations! You've guessed the word:" + current_word)
-        return True
+            print("\nCongratulations! You've guessed the word:", current_word)
+            return True
+        return False
     else:
         return False
 
 
 def main():
-    """The main function to run the Hangman game."""
-    amount_of_times_wrong = 0
-    current_letters_guessed = []
-    current_letters_right = 0
+    play_again = True
+    while play_again:
+        amount_of_times_wrong = 0
+        current_letters_guessed = []
 
-    print(WELCOME_MESSAGE)
+        print(WELCOME_MESSAGE)
 
-    current_word = random.choice(WORD_DICTIONARY)
-    current_word = current_word.lower()
-    print_empty_lines(current_word)
+        current_word = random.choice(WORD_DICTIONARY)
+        current_word = current_word.lower()
 
-    while amount_of_times_wrong != MAX_ATTEMPTS:
-        print_current_guessed(current_letters_guessed)
-        user_input = input_next_guess(current_letters_guessed)
+        while amount_of_times_wrong != MAX_ATTEMPTS:
+            print_current_word(current_word, current_letters_guessed)
+            print_empty_lines(current_word)
+            print_current_guessed(current_letters_guessed)
 
-        is_input_valid = check_guessed_word(
-            current_letters_guessed,
-            current_word,
-            user_input,
-            current_letters_right,
-        )
+            user_input = input_next_guess(current_letters_guessed)
+            is_input_valid = check_guessed_word(current_letters_guessed, current_word, user_input)
 
-        if not is_input_valid:
-            amount_of_times_wrong += 1
-            print_hangman(amount_of_times_wrong)
+            if is_input_valid:
+                break
+            else:
+                if user_input not in current_word:
+                    amount_of_times_wrong += 1
+                    if amount_of_times_wrong != MAX_ATTEMPTS:
+                        print_hangman(amount_of_times_wrong)
+                    if amount_of_times_wrong == MAX_ATTEMPTS:
+                        print("\nSorry, you've run out of attempts. The word was:", current_word)
+                        break
 
-    if amount_of_times_wrong == MAX_ATTEMPTS:
-        print(
-            "\nSorry, you've run out of attempts. The word was:", current_word
-        )
-
-    print("Game is over! Thank you for playing :)")
+        play_again_input = ""
+        while play_again_input not in ["y", "n"]:
+            play_again_input = input("\nDo you want to play again? (Y/N): ").lower()
+            if play_again_input == "n":
+                print("\nThanks for playing!\n")
+                play_again = False
+                break
 
 
 if __name__ == "__main__":
